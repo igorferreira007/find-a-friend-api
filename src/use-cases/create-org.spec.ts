1c2d6e69b929aa909582a4eb8f3800702784df75
Igor Ferreira <ferreiraigor007@gmail.com>
2025-11-26 20:32:49 -0300

    Added new test to not create an org

--- src/use-cases/create-org.spec.ts
+++ src/use-cases/create-org.spec.ts
@@ -1,9 +1,9 @@
 import { beforeEach, describe, expect, it } from "vitest"
-import { OrgsRepository } from "@/repositories/orgs-repositories.ts"
 import { InMemoryOrgsRepository } from "@/repositories/in-memory/in-memory-orgs-repository.ts"
 import { CreateOrgUseCase } from "./create-org.ts"
+import { OrgAlreadyExistsError } from "./erros/org-already-exists-error.ts"
 
-let orgsRepository: OrgsRepository
+let orgsRepository: InMemoryOrgsRepository
 let sut: CreateOrgUseCase
 
 describe("Create org use case", () => {
@@ -13,7 +13,7 @@ describe("Create org use case", () => {
   })
 
   it("should be able to create org", async () => {
-    const org = await orgsRepository.create({
+    const { org } = await sut.execute({
       name: "Find a friend",
       coordinatorName: "John Doe",
       email: "johndoe@email.com",
@@ -29,5 +29,39 @@ describe("Create org use case", () => {
     })
 
     expect(org.id).toEqual(expect.any(String))
+  })
+
+  it("should not be able to create org", async () => {
+    await sut.execute({
+      name: "Find a friend",
+      coordinatorName: "John Doe",
+      email: "johndoe@email.com",
+      password: "123456",
+      whatsapp: "00000000000",
+      cep: "12220-610",
+      state: "SP",
+      city: "São José dos Campos",
+      neighborhood: "Vila Tatetuba",
+      street: "Rua 01",
+      latitude: -23.21853715183315,
+      longitude: -45.808786372274426,
+    })
+
+    await expect(() =>
+      sut.execute({
+        name: "Find a friend",
+        coordinatorName: "John Doe",
+        email: "johndoe@email.com",
+        password: "123456",
+        whatsapp: "00000000000",
+        cep: "12220-610",
+        state: "SP",
+        city: "São José dos Campos",
+        neighborhood: "Vila Tatetuba",
+        street: "Rua 01",
+        latitude: -23.21853715183315,
+        longitude: -45.808786372274426,
+      })
+    ).rejects.toBeInstanceOf(OrgAlreadyExistsError)
   })
 })
