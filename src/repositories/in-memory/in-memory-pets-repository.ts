1ef30c082b1ba2a5a80f452a437cb1670df5df96
Igor Ferreira <ferreiraigor007@gmail.com>
2025-11-26 20:27:25 -0300

    Added the method findAll and the constructor for dependency of orgsRepository

--- src/repositories/in-memory/in-memory-pets-repository.ts
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -1,9 +1,36 @@
 import { Pet, Prisma } from "@prisma/client"
-import { PetsRepository } from "../pets-repositories.ts"
+import { FindAllParams, PetsRepository } from "../pets-repositories.ts"
 import { randomUUID } from "node:crypto"
+import { InMemoryOrgsRepository } from "./in-memory-orgs-repository.ts"
 
 export class InMemoryPetsRepository implements PetsRepository {
   public items: Pet[] = []
+
+  constructor(private orgsRepository: InMemoryOrgsRepository) {}
+
+  async findAll(params: FindAllParams): Promise<Pet[]> {
+    const orgsByCity = this.orgsRepository.items.filter(
+      (org) => org.city === params.city
+    )
+
+    const pets = this.items
+      .filter((pet) => orgsByCity.some((org) => org.id === pet.orgId))
+      .filter((pet) => (params.age ? pet.age === params.age : true))
+      .filter((pet) => (params.size ? pet.size === params.size : true))
+      .filter((pet) =>
+        params.energyLevel ? pet.energyLevel === params.energyLevel : true
+      )
+      .filter((pet) =>
+        params.independenceLevel
+          ? pet.independenceLevel === params.independenceLevel
+          : true
+      )
+      .filter((pet) =>
+        params.environment ? pet.environment === params.environment : true
+      )
+
+    return pets
+  }
 
   async create(data: Prisma.PetUncheckedCreateInput): Promise<Pet> {
     const pet: Pet = {
