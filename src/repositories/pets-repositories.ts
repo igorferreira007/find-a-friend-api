20da52c56d70fc37d0f55f647637b151c9f4872c
Igor Ferreira <ferreiraigor007@gmail.com>
2025-11-26 20:29:03 -0300

    Added the method findAll in interface PetsRepository

--- src/repositories/pets-repositories.ts
+++ src/repositories/pets-repositories.ts
@@ -1,5 +1,15 @@
-import { Pet, Prisma } from "@prisma/client"
+import { Age, Level, Pet, Prisma, Size } from "@prisma/client"
+
+export interface FindAllParams {
+  city: string
+  age?: Age
+  size?: Size
+  energyLevel?: Level
+  independenceLevel?: Level
+  environment?: Size
+}
 
 export interface PetsRepository {
+  findAll(params: FindAllParams): Promise<Pet[]>
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
 }
